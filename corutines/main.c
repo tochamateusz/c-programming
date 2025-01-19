a03f3a566a2f608a2f351bef3eb29416778cc6ae
tocha_mateusz <mateusz@primea.se>
2025-01-19 11:21:17 +0100

    fix(corutines): rewirte macro

--- corutines/main.c
+++ corutines/main.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+
 typedef struct coroutine_t {
   float elapsed;
   int index;
@@ -9,19 +10,20 @@ typedef struct coroutine_t {
   do {                                                                         \
     switch (co->index) {                                                       \
     default:
-
 #define COROUTINE_CASE(co, name)                                               \
   case __LINE__:                                                               \
   name:                                                                        \
     co->index = __LINE__;
-
+#define COROUTINE_EXIT(co)                                                     \
+  do {                                                                         \
+    goto __co_end;                                                             \
+  } while (0)
 #define COROUTINE_YIELD(co)                                                    \
   do {                                                                         \
     co->index = __LINE__;                                                      \
     COROUTINE_EXIT(co);                                                        \
   case __LINE__:;                                                              \
   } while (0)
-
 #define COROUTINE_WAIT(co, time, dt)                                           \
   do {                                                                         \
   case __LINE__:                                                               \
@@ -35,12 +37,6 @@ typedef struct coroutine_t {
       }                                                                        \
     } while (0);                                                               \
   } while (0)
-
-#define COROUTINE_EXIT(co)                                                     \
-  do {                                                                         \
-    goto __co_end;                                                             \
-  } while (0)
-
 #define COROUTINE_END(co)                                                      \
   }                                                                            \
   co->index = 0;                                                               \
