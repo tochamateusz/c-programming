f8951c4f5fffe8e9dc3325e80949840924c852ad
tocha_mateusz <mateusz@primea.se>
2025-01-19 11:43:35 +0100

    chore(corutines): fix format

--- corutines/main.c
+++ corutines/main.c
@@ -9,20 +9,24 @@ typedef struct coroutine_t {
   do {                                                                         \
     switch (co->index) {                                                       \
     default:
+
 #define COROUTINE_CASE(co, name)                                               \
   case __LINE__:                                                               \
   name:                                                                        \
     co->index = __LINE__;
+
 #define COROUTINE_EXIT(co)                                                     \
   do {                                                                         \
     goto __co_end;                                                             \
   } while (0)
+
 #define COROUTINE_YIELD(co)                                                    \
   do {                                                                         \
     co->index = __LINE__;                                                      \
     COROUTINE_EXIT(co);                                                        \
   case __LINE__:;                                                              \
   } while (0)
+
 #define COROUTINE_WAIT(co, time, dt)                                           \
   do {                                                                         \
   case __LINE__:                                                               \
@@ -36,6 +40,7 @@ typedef struct coroutine_t {
       }                                                                        \
     } while (0);                                                               \
   } while (0)
+
 #define COROUTINE_END(co)                                                      \
   }                                                                            \
   co->index = 0;                                                               \
